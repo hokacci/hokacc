f4e1313ca46c23c467d2275712b908d0cb339269
Yoshihiro Hokazono <yoshihiro.hokazono@pixiedusttech.com>
2022-05-06 00:29:52 +0900

    Add test

--- test/test.py
+++ test/test.py
@@ -42,6 +42,7 @@ def main():
     assert(test("a = 1;", 1))
     assert(test("a = 102; b = 2; a;", 102))
     assert(test("aiko = 1; becky = 2; aiko + becky == 3;", 1))
+    assert(test("S_var = 25; t__123=22; S_var + t__123;", 47))
     print("******** All tests passed! ********")
 
 
